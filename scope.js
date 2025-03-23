d987b6b3c800fd3b4ee464c3a59fc995451bbc4c
Sagar Nepali <sagarnepali1100@gmail.com>
2025-03-23 22:42:21 +0545

    file upload

--- scope.js
+++ scope.js
@@ -1,1 +1,19 @@
+// Scope 
+/**
+ * What is Scope?
+ * SCOPE BASICALLY MEAN A CERTAIN AREA WHERE IT WORK AND NOT WORK.
+ * like a variable, VARIABLE INSIDE THE FUNCTION DOESN'T WORK OUTSIDE THAT FUNCTION.
+ */
 
+
+// let age = 21; this is called global scope
+// => GLOBAL SCOPE, THAT VARIABLE VALUE CAN WORK WITHIN ALL THE AREA OF CODE.
+let age = 21; 
+function userAge(){
+    // let age = 40; this is block scope
+    // => BLOCK SCOPE, ONLY RUN WITHIN THE BLOCK, IT'S VALUE DOESN'T WORK OUTSIDE THE BLOCK. 
+    let age = 40;
+    console.log(age) 
+    // OUTPUT: 40
+}
+userAge()
